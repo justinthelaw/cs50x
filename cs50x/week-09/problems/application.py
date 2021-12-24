b779c875d98474aba69b9c832b81060d9b65d9f7
justinthelaw <justinwingchunglaw@gmail.com>
2021-12-24 16:43:25 -0500

    add comments to CS50 finanace

--- cs50x/week-09/problems/application.py
+++ cs50x/week-09/problems/application.py
@@ -6,7 +6,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.sansio.response import Response
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
@@ -121,6 +120,7 @@ def buy():
                     currentCash - cost,
                     userID,
                 )
+                # insert transaction into history
                 db.execute(
                     "INSERT INTO history (action, symbol, companyName, latestPrice, userID, shares) VALUES(?, ?, ?, ?, ?, ?)",
                     "BUY",
@@ -310,6 +310,7 @@ def sell():
                     currentCash + cost,
                     userID,
                 )
+                # insert transaction into history
                 db.execute(
                     "INSERT INTO history (action, symbol, companyName, latestPrice, userID, shares) VALUES(?, ?, ?, ?, ?, ?)",
                     "SELL",
